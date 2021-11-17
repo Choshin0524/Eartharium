b3dc4712ec79b6b548e0b3457596d8a43f8dc5b2
Chosin <smzhg0524@gmail.com>
2021-11-18 00:50:30 +0900

    2st commit

--- get_weather.py
+++ get_weather.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
-
+import re
 
 def get_weather_info():
     CHROME_DRIVER = '/usr/lib/chromium-browser/chromedriver'
@@ -15,8 +15,13 @@ def get_weather_info():
     tempele_today_high = browser.find_element_by_class_name("high")
     tempele_today_low = browser.find_element_by_class_name("low")
     weather_today = browser.find_element_by_class_name("pict")
-    #天気情報を取り出し配列に格納
     
-    weather_info_array = [tempele_today_high.text[0:2],tempele_today_low.text[0:2],weather_today.text]
+    #天気情報を取り出し配列に格納（正規表現re利用）
+    temp_regex = re.compile(r'\d+') #気温情報のみ取り出すため
+    temp_today_high_match = temp_regex.search(tempele_today_high.text)
+    temp_today_low_match = temp_regex.search(tempele_today_low.text)
+    weather_info_array = [temp_today_high_match.groups(),temp_today_low_match.groups(),weather_today.text]
+    
+    #終了
     print("天気情報取得OK.")
     return weather_info_array
