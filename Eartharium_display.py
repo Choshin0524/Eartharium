8c26dee6627a6b82c5bada086dbb677cb99e778a
Chosin <smzhg0524@gmail.com>
2021-11-18 05:08:37 +0900

    ver 0.0.2

--- Eartharium_display.py
+++ Eartharium_display.py
@@ -11,19 +11,7 @@ import datetime
 weather_list = []
 time_now = ""
 
-#LCDディスプレイのセットアップ
-if __name__ == "__main__":
-    PCF8574_address = 0x27
-    mcp = PCF8574_GPIO(PCF8574_address)
-    lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
-    mcp.output(3,1)
-    lcd.begin(16,2)
-    lcd.setCursor(3,0)
-    lcd.message("Welcome to")
-    lcd.setCursor(3,1)
-    lcd.message("Earthrium")
-    time.sleep(2)
-    lcd.clear()
+
 
 def get_weather_list():
     global weather_list
@@ -41,20 +29,43 @@ def show_weather():
     lcd.setCursor(8,0)
     lcd.message(weather_list[2])
     lcd.setCursor(0,1)
-    lcd.message(str(weather_list[0])+"  "+str(weather_list[1]))
+    lcd.message("Low:"+str(weather_list[0])+"℃ High:"+str(weather_list[1])+"℃")
+
+def destroy():
+    lcd.clear()
+
+#LCDディスプレイのセットアップ
+if __name__ == "__main__":
+    PCF8574_address = 0x27
+    mcp = PCF8574_GPIO(PCF8574_address)
+    lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+    mcp.output(3,1)
+    lcd.begin(16,2)
+    lcd.setCursor(3,0)
+    lcd.message("Welcome to")
+    lcd.setCursor(3,1)
+    lcd.message("Earthrium")
+    time.sleep(2)
+    lcd.clear()
+    get_weather_list()
 
 #定時実行のスケジューリング
 schedule.every(1).seconds.do(get_time)
 schedule.every(2).minutes.do(get_weather_list)
 
-while True:
-    schedule.run_pending()
-    show_time()
-    show_weather()
-    time.sleep(1)
-    lcd.clear()
+def loop():
+    while True:
+        schedule.run_pending()
+        show_time()
+        show_weather()
+
+if __name__ == "__main__":
     
-    
+    print ('Earthrium is starting ... ')
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
     
     
 
